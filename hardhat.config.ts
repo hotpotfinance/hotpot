a70cab8c9ba4015e9b845bdffa8df32eefbb2b49
nicentive <nic@bincentive.com>
2021-08-06 09:40:08 +0800

    Commit config files

---
+++ hardhat.config.ts
@@ -0,0 +1,62 @@
+import { task } from "hardhat/config"
+import { ethers } from "hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-ethers"
+
+import { mnemonic } from './secrets.json';
+
+export default {
+    // defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545"
+        },
+        hardhat: {
+            chainId: 56,
+            forking: {
+                // url: "https://bsc-dataseed.binance.org/",
+                //   blockNumber: 9704575
+            }
+        },
+        testnet: {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+            chainId: 97,
+            accounts: { mnemonic: mnemonic }
+        },
+        mainnet: {
+            url: "https://bsc-dataseed.binance.org/",
+            chainId: 56,
+            accounts: { mnemonic: mnemonic }
+        }
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.5.16",
+                settings: {
+                    optimizer: {
+                        enabled: true
+                    }
+                }
+            },
+            {
+                version: "0.8.4",
+                settings: {
+                    optimizer: {
+                        enabled: true
+                    }
+                }
+            },
+        ]
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts"
+    },
+    mocha: {
+        timeout: 100000
+    }
+}
+
